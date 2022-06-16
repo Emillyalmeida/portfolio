3db8296bce6e9a5ffdaa0c81bf55fa38c94dd8f9
emillyalmeida <emillyalmeidan@gmail.com>
2022-06-16 15:30:36 -0300

    feat(theme): create themes

---
+++ src/styles/theme.js
@@ -0,0 +1,34 @@
+const lightTheme = {
+  color: "#001233",
+  backgroundColor: "#61a5c2",
+  card: {
+    backgroundColor: "#2a6f97",
+  },
+  header: {},
+  button: {
+    textColor: "#61a5c2",
+    backgroundColor: "#001845",
+    hover: "#01497c",
+  },
+};
+
+const darkTheme = {
+  color: "#89c2d9",
+  backgroundColor: "#001845",
+
+  card: {
+    backgroundColor: "#023e7d",
+  },
+  header: {},
+
+  button: {
+    textColor: "#89c2d9",
+    backgroundColor: "#0466c8",
+    hover: "#002855",
+  },
+};
+
+export const themes = {
+  light: lightTheme,
+  dark: darkTheme,
+};
