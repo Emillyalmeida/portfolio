c0a12aacc39b5f0f9a55a62d0f53a7961fe5c26a
emillyalmeida <emillyalmeidan@gmail.com>
2022-06-20 18:31:57 -0300

    feat(footer): create component footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,11 @@
+import { DivFooter } from "./styles";
+
+const Footer = () => {
+  return (
+    <DivFooter>
+      <h3> &copy;Emilly Almeida</h3>
+    </DivFooter>
+  );
+};
+
+export default Footer;
---
+++ src/components/footer/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const DivFooter = styled.footer`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme.terciary};
+`;
