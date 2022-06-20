7749a43dbbdb102fb9f28f0a5484a36a17af0d33
emillyalmeida <emillyalmeidan@gmail.com>
2022-06-20 18:33:00 -0300

    feat(about): init page aboutme

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,5 +1,7 @@
 import Container from "../../components/container";
 import Title from "../../components/Title";
+import { Saudation } from "../contact/style";
+import imgAnime from "../../assets/myanime.png";
 
 const AboutMe = () => {
   return (
@@ -7,6 +9,16 @@ const AboutMe = () => {
       <Title>
         <h1> Sobre mim</h1>
       </Title>
+      <Saudation>
+        <div>
+          <p>
+            Eu sou Desenvolvedora Fron-end, vivo no Brasil no estado da Bahia.
+            Amo animais , principalmente gatos, tenho um que se chama Bichano.
+            Sou a apaixonda por livros de ficção.
+          </p>
+        </div>
+        <img src={imgAnime} alt="" />
+      </Saudation>
     </Container>
   );
 };
