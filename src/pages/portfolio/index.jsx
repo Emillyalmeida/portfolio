21e0b43e53cf27a2d8a318454235150f90a29813
emillyalmeida <emillyalmeidan@gmail.com>
2022-06-22 16:41:00 -0300

    feat(portfolio): add react carrosel

--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -1,9 +1,15 @@
 import { useContext } from "react";
+
 import Card from "../../components/card";
 import Container from "../../components/container";
 import Title from "../../components/Title";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
 import { ListProjectsContext } from "../../providers/projects";
-import { ListUl } from "./style";
+import { ListUl, Slide } from "./style";
+import Footer from "../../components/footer";
 
 const Portfolio = () => {
   const { listProjects } = useContext(ListProjectsContext);
@@ -12,11 +18,21 @@ const Portfolio = () => {
       <Title>
         <h1>Portfolio</h1>
       </Title>
+      <Carousel showThumbs={false}>
+        {listProjects.map((project) => (
+          <Slide key={project.id} img={project.img}>
+            <div>
+              <h2>{project.name}</h2>
+            </div>
+          </Slide>
+        ))}
+      </Carousel>
       <ListUl>
         {listProjects.map((project) => (
           <Card project={project} key={project.id} />
         ))}
       </ListUl>
+      <Footer />
     </Container>
   );
 };
--- src/pages/portfolio/style.js
+++ src/pages/portfolio/style.js
@@ -1,10 +1,40 @@
 import styled from "styled-components";
 
 export const ListUl = styled.ul`
-  width: 100%;
+  width: 90%;
   display: flex;
   justify-content: space-evenly;
   flex-wrap: wrap;
   gap: 10px;
-  padding: 20px;
+  padding: 50px 20px;
+`;
+
+export const Slide = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 350px;
+  width: 100%;
+  margin: 0;
+  font-size: 4em;
+  background-image: url(${(props) => props.img});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  margin-bottom: 50px;
+
+  div {
+    backdrop-filter: blur(4px);
+    height: 350px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    h2 {
+      padding-left: 20px;
+      font-size: 3.5rem;
+      font-weight: 700;
+      text-shadow: -3px 2px 4px ${(props) => props.theme.backgroundColor};
+    }
+  }
 `;
